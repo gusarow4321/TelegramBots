cdc9ae6c4af923190be185c846b67c1b2bf86d9c
Heinlein1957 <40901234+Heinlein1957@users.noreply.github.com>
2018-07-20 10:15:06 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,47 +1,57 @@
-from config import token
 import telebot
-import random
 
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot("600150670:AAEvKgOCOBHBZ9NLzF4XWZft_xF2x6Q9tRU")
 
-players = {}
+users_arr = dict()
+
+keyb = telebot.types.InlineKeyboardMarkup()
+button_plus = telebot.types.InlineKeyboardButton(text='+', callback_data='1')
+button_minus = telebot.types.InlineKeyboardButton(text='-', callback_data='0')
+keyb.add(button_plus, button_minus)
+
+t = []
+with open('1.txt', 'r') as file:
+    for row in file.readlines():
+        t.append(row.strip())
 
 
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-    print(message.from_user.first_name + ': ' + message.text)
-    bot.send_message(message.from_user.id, "\U0001F3AE Game is started\nFor end a game send \stop")
-    players[message.from_user.id] = {'name': message.from_user.first_name, 'score': 0}
-    bot.send_message(message.from_user.id, 'Choose 5 numbers ranging from 1 to 20.\nFor example: 1 2 3 4 5')
+    bot.send_message(message.from_user.id, text='Тест будет состоять из четырех блоков, в каждом из которых по 20 '
+                                                'свойств, характерных для представителей каждого типа '
+                                                'темперамента.\n\nВнимательно прочитайте каждое свойство и поставьте '
+                                                'знак (+), если считаете, что это свойство Вам присуще, и знак (–) – '
+                                                'если оно у Вас отсутствует\n\nОтправьте /continue когда будете готовы')
 
 
-@bot.message_handler(commands=['stop'])
-def stop_handler(message):
-    print(message.from_user.first_name + ': ' + message.text)
-    bot.send_message(message.from_user.id, f'Game over\nYour final score: {players[message.from_user.id]["score"]}')
-    del players[message.from_user.id]
+@bot.message_handler(commands=['continue'])
+def test_handler(message):
+    users_arr[message.from_user.id] = [0, 0, 0, 0]
+    bot.send_message(message.from_user.id, t[0], reply_markup=keyb)
 
 
-@bot.message_handler(content_types=['text'])
-def game(message):
-    print(message.from_user.first_name + ': ' + message.text)
-    if message.from_user.id in players:
-        arr = [str(random.randrange(1, 21)) for i in range(5)]
-        bot.send_message(message.from_user.id, 'Winning numbers: ' + ' '.join(arr))
-        player_arr = message.text.split(' ')
-        correct_nums = 0
-        for val in arr:
-            if val in player_arr:
-                correct_nums += 1
-        players[message.from_user.id]["score"] += correct_nums
-        if correct_nums > 0:
-            bot.send_message(message.from_user.id, f'\U0001F389 You guessed {correct_nums} numbers\n'
-                                                   f'Your score is {players[message.from_user.id]["score"]}')
-        else:
-            bot.send_message(message.from_user.id, '\U0001F62D You lose')
-    else:
-        bot.send_message(message.from_user.id, 'Use command /start to start the game')
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    ind = t.index(call.message.text) + 1
+    data = call.data
+    type_temp = call.message.text.split('.')[0]
+    users_arr[call.from_user.id][int(type_temp) - 1] += int(data)
+    try:
+        bot.edit_message_text(t[ind], call.from_user.id, message_id=call.message.message_id, reply_markup=keyb)
+    except IndexError:
+        h = round((users_arr[call.from_user.id][0] / sum(users_arr[call.from_user.id])) * 100)
+        s = round((users_arr[call.from_user.id][1] / sum(users_arr[call.from_user.id])) * 100)
+        f = round((users_arr[call.from_user.id][2] / sum(users_arr[call.from_user.id])) * 100)
+        m = round((users_arr[call.from_user.id][3] / sum(users_arr[call.from_user.id])) * 100)
+        mes = 'Результаты теста показывают, что ваш темперамент на {}% – холерический, {}% – сангвинический, ' \
+              '{}% – флегматический и {}% – меланхолический.\n\n• Если относительный результат числа положительных ' \
+              'ответов по какому-либо типу составляет 40% и выше, значит, данный тип темперамента у вас ' \
+              'доминирующий.\n• Если этот результат составляет 30–39%, то качества данного типа выражены достаточно ' \
+              'ярко.\n• Если результат 20–29%, то средне выражены.\n• При результате 10–19% можно утверждать, ' \
+              'что черты этого типа темперамента выражены в малой степени.\n\nДля повторного прохождения теста ' \
+              'отправьте /continue'.format(str(h), str(s), str(f), str(m))
+        bot.edit_message_text(mes, call.from_user.id, message_id=call.message.message_id)
+        del users_arr[call.from_user.id]
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+bot.polling(none_stop=True)
