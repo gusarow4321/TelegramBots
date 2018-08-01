bd750e865af31b86ec98f4d60ccb6ca761b921c9
Heinlein1957 <gusarow4321@gmail.com>
2018-08-01 12:05:58 +0300

    new

--- Mystery_House_Bot/database.py
+++ Mystery_House_Bot/database.py
@@ -2,21 +2,51 @@ import sqlite3
 
 
 def add_player(user_id, name):
-    progress = "S_0"
+    progress = "0_0"
+    inv = "test"
     db = sqlite3.connect("players.db")
     cur = db.cursor()
     if not cur.execute(f"SELECT * FROM players WHERE user_id={user_id}").fetchall():
         with db:
-            cur.execute(f"INSERT INTO players (user_id, name, progress) VALUES ({user_id}, '{name}', '{progress}')")
+            cur.execute(f"INSERT INTO players (user_id, name, progress, invent) VALUES ({user_id}, '{name}', "
+                        f"'{progress}', '')")
     db.close()
 
 
-def get_img(user_id):
+def get_progress(user_id):
     db = sqlite3.connect("players.db")
     cur = db.cursor()
     with db:
         img_name = cur.execute(f"SELECT progress FROM players WHERE user_id={user_id}").fetchall()
+    db.close()
     return str(img_name[0][0])
+
+
+def update_progress(user_id, p):
+    db = sqlite3.connect("players.db")
+    cur = db.cursor()
+    with db:
+        cur.execute(f"UPDATE players SET progress='{p}' WHERE user_id={user_id}").fetchall()
+    db.close()
+
+
+def get_inv(user_id):
+    db = sqlite3.connect("players.db")
+    cur = db.cursor()
+    with db:
+        tool = cur.execute(f"SELECT invent FROM players WHERE user_id={user_id}").fetchall()
+    db.close()
+    return str(tool[0][0])
+
+
+def add_to_inv(user_id, tool):
+    inv = get_inv(user_id)
+    db = sqlite3.connect("players.db")
+    cur = db.cursor()
+    inv += tool + "_"
+    with db:
+        cur.execute(f"UPDATE players SET invent='{inv}' WHERE user_id={user_id}").fetchall()
+    db.close()
 
 
 if __name__ == "__main__":
--- Mystery_House_Bot/main.py
+++ Mystery_House_Bot/main.py
@@ -5,9 +5,29 @@ import database
 bot = telebot.TeleBot(config.token)
 
 
-def send_img(user_id, img_name):
+def create_markup(t):
+    m = telebot.types.InlineKeyboardMarkup()
+    for b in t:
+        m.add(telebot.types.InlineKeyboardButton(b[0], callback_data=b[1]))
+    return m
+
+
+def send_img(user_id, img_name, markup):
     with open("img\\" + img_name + ".png", 'rb') as photo:
-        bot.send_photo(user_id, photo, config.texts[img_name])
+        bot.send_photo(user_id, photo, config.texts[img_name], reply_markup=markup)
+
+
+def moving(call, updown):
+    user_pr = database.get_progress(call.from_user.id)
+    coord = user_pr.split("_")
+    if updown:
+        update = str(int(coord[0]) + int(call.data.split("_")[0])) + "_" + coord[1]
+    else:
+        update = coord[0] + "_" + str(int(coord[1]) + int(call.data.split("_")[2]))
+    if user_pr == "1_0_0":
+        update += "_0"
+    database.update_progress(call.from_user.id, update)
+    send_img(call.from_user.id, update, create_markup(config.marks[update]))
 
 
 @bot.message_handler(commands=['start'])
@@ -21,14 +41,48 @@ def start_handler(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
+    if call.message.content_type == 'text':
+        bot.edit_message_text(call.message.text, call.from_user.id, call.message.message_id)
+    elif call.message.content_type == 'photo':
+        bot.edit_message_caption(call.message.caption, call.from_user.id, call.message.message_id)
+
     if call.data == "start game":
         database.add_player(call.from_user.id, call.from_user.first_name)
-        send_img(call.from_user.id, database.get_img(call.from_user.id))
-
+        user_pr = database.get_progress(call.from_user.id)
+        send_img(call.from_user.id, database.get_progress(call.from_user.id), create_markup(config.marks[user_pr]))
     elif call.data == "about":
         ab_markup = telebot.types.InlineKeyboardMarkup()
         ab_markup.add(telebot.types.InlineKeyboardButton("\U0001F3AE Начать игру", callback_data="start game"))
         bot.send_message(call.from_user.id, config.about, reply_markup=ab_markup)
+    elif call.data == "info":
+        u_p = database.get_progress(call.from_user.id)
+        bot.send_message(call.from_user.id, config.explore[u_p], reply_markup=create_markup(config.marks[u_p][1:]))
+    elif "add" in call.data:
+        database.add_to_inv(call.from_user.id, call.data.split(" ")[1])
+        coord = call.data.split(" ")[2]
+        send_img(call.from_user.id, coord, create_markup(config.marks[coord]))
+    elif "_" in call.data:
+        if call.data[0] != "_":
+            moving(call, True)
+        else:
+            moving(call, False)
+    elif call.data == "door":
+        user_pr = database.get_progress(call.from_user.id)
+        if user_pr == "1_0":
+            database.update_progress(call.from_user.id, "1_0_0")
+            send_img(call.from_user.id, "1_0_0", create_markup(config.marks["1_0_0"]))
+        elif user_pr == "2_-2":
+            bot.send_message(call.from_user.id, config.no_exit, reply_markup=create_markup(config.marks[user_pr][1:]))
+    elif call.data == "read note":
+        user_pr = database.get_progress(call.from_user.id)
+        if user_pr == "2_0_0":
+            database.update_progress(call.from_user.id, "2_0_1")
+            send_img(call.from_user.id, "note_1", create_markup(config.marks["note"]))
+    elif call.data == "drop note":
+        user_pr = database.get_progress(call.from_user.id)
+        send_img(call.from_user.id, database.get_progress(call.from_user.id), create_markup(config.marks[user_pr]))
+    else:
+        send_img(call.from_user.id, call.data, create_markup(config.marks[call.data]))
 
 
 if __name__ == "__main__":
